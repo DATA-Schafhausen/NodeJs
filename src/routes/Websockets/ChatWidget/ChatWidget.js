60bddf41cd67fc51d6994bf6bc4105aae280270b
Fernando <viktorseidl@gmail.com>
2024-03-29 22:02:14 +0100

    websocket implementation

--- src/routes/Websockets/ChatWidget/ChatWidget.js
+++ src/routes/Websockets/ChatWidget/ChatWidget.js
@@ -6,7 +6,7 @@ module.exports = function(io) {
       socket.on('connect_on_unit_room', (unitId,userId,uname) => {
         // Handle subscribing to notifications for a user
         const room=getUnitRoom(unitId)
-        socket.emit('connect_on_unit_room', room)
+        socket.emit('connect_on_unit_room', `Welcome, User ${unitId}!`)
         // Emit personalized notifications to the client
         //socket.emit('personalized_notification', `Welcome, User ${userId}!`);
       });
