b0d9f0c788d6f1b9021d4a688c16ca3b4d6fb3e7
Fernando <viktorseidl@gmail.com>
2024-03-29 21:58:48 +0100

    websocket implementation

--- src/routes/Websockets/ChatWidget/ChatWidget.js
+++ src/routes/Websockets/ChatWidget/ChatWidget.js
@@ -1,7 +1,12 @@
+const { getUnitRoom } = require("./RoomManager");
+
 module.exports = function(io) {
     io.on('connection', (socket) => {
-      socket.on('subscribe_to_notifications', (userId) => {
+      
+      socket.on('connect_on_unit_room', (unitId,userId,uname) => {
         // Handle subscribing to notifications for a user
+        const room=getUnitRoom(unitId)
+        socket.emit('connect_on_unit_room', room)
         // Emit personalized notifications to the client
         //socket.emit('personalized_notification', `Welcome, User ${userId}!`);
       });
--- src/routes/Websockets/ChatWidget/RoomManager.js
+++ src/routes/Websockets/ChatWidget/RoomManager.js
@@ -1,15 +1,31 @@
 const rooms={}
-
+/**
+ * GET UNIT SOCKET ROOM CONNECTIONS
+ * @param {*} unitId UNIT UUID 
+ * @returns Room 
+ */
 export function getUnitRoom(unitId){
     if(!rooms[unitId]){
         rooms[unitId]={}
     }
         return rooms[unitId]
 }
+/**
+ * CHECK IF USER ALREADY EXISTS
+ * @param {*} unitId 
+ * @param {*} userId 
+ * @returns 
+ */
 export function checkUser(unitId,userId){
     const is=rooms[unitId][userId]?true:false
     return is
 }
+/**
+ * IF USER IS NOT EXISTENT THEN ADD HIM TO THE UNIT
+ * @param {*} unitId 
+ * @param {*} userId 
+ * @param {*} uname 
+ */
 export function addUserToUnitRoom(unitId,userId,uname){
     if(!rooms[unitId]){
         rooms[unitId]={}
@@ -29,4 +45,23 @@ export function addUserToUnitRoom(unitId,userId,uname){
         connectTime:new Date().getTime(),
         disconnectTime:0
     }
+}
+export function disconnectUserFromChart(unitId,userId,uname){
+    if(!rooms[unitId]){
+        rooms[unitId]={}
+    }
+    if(!checkUser(userId)) rooms[unitId][userId]={
+        id:userId,
+        uname:uname,
+        state:false,
+        connectTime:0,
+        disconnectTime:0
+    } 
+    rooms[unitId][userId]={
+        id:userId,
+        uname:uname,
+        state:false,
+        connectTime:0,
+        disconnectTime:new Date().getTime()
+    } 
 }
