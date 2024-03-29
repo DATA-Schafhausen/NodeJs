fec3036cb143ede1cc622577f5102edbd9b13045
Fernando <viktorseidl@gmail.com>
2024-03-29 22:13:40 +0100

    websocket implementation

--- src/routes/Websockets/ChatWidget/ChatWidget.js
+++ src/routes/Websockets/ChatWidget/ChatWidget.js
@@ -3,12 +3,12 @@ const { getUnitRoom } = require("./RoomManager");
 module.exports = function(io) {
     io.on('connection', (socket) => {
       
-      socket.on('connect_on_unit_room', (unitId,userId,uname) => {
+      //socket.on('connect_on_unit_room', (unitId,userId,uname) => {
         // Handle subscribing to notifications for a user
-        const room=getUnitRoom(unitId)
-        socket.emit('connect_on_unit_room', `Welcome, User ${unitId}!`)
+        //const room=getUnitRoom(unitId)
+        //socket.emit('connect_on_unit_room', `Welcome, User ${unitId}!`)
         // Emit personalized notifications to the client
         //socket.emit('personalized_notification', `Welcome, User ${userId}!`);
-      });
+      //});
     });
   };
